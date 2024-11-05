e5dbe433aebf9b0ce49c35ae83e020f3037a4fbe
Emma Strienko <mstrienko@gmail.com>
2024-11-05 16:52:17 +0200

    user activation

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,13 +1,9 @@
-import express from 'express';
-import { registrationUser } from '../controllers/user.controller';
+import express from "express";
+import { activateUser, registrationUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
-userRouter.post('/registration', registrationUser)
+userRouter.post("/registration", registrationUser);
+
+userRouter.post("/activate_user", activateUser);
 
 export default userRouter;
-
-
-
-
-
-
