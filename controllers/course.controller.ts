818730407e191067fd9468f5d3ebe9ec9314f7b2
Emma Strienko <mstrienko@gmail.com>
2024-11-25 16:40:12 +0200

    get all notifications, update notification status

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -10,6 +10,7 @@ import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
 import { error } from "console";
+import NotificationModel from "../models/notification.model";
 
 // upload course
 export const uploadCourse = CatchAsyncError(
@@ -203,6 +204,12 @@ export const addQuestion = CatchAsyncError(
       // add this question to course content
       courseContent.questions.push(newQuestion);
 
+      await NotificationModel.create({
+        user: req.user?._id,
+        title: "New Question Recieved",
+        message: `You have a new question in ${courseContent.title}`
+      })
+
       // save the updated course
       await course?.save();
 
@@ -263,6 +270,11 @@ export const addAnswer = CatchAsyncError(
 
       if (req.user?._id === question.user._id) {
         // create a notification
+        await NotificationModel.create({
+          user: req.user?._id,
+          title: "New Question Reply Received",
+          message: `You have a new question reply in ${courseContent.title}`
+        })
       } else {
         const data = {
           name: question.user.name,
